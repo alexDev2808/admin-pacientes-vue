db822375c975afe0a111dda7e6b66b780ebbdb56
alexDev2808 <colores.2808.8000@gmail.com>
2023-12-14 10:38:03 -0600

    Agregando id unico

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,7 @@
 <script setup>
   import { ref, reactive } from 'vue';
+  import { uid } from 'uid';
+
   import Header from './components/Header.vue';
   import Formulario from './components/Formulario.vue';
   import Paciente from './components/Paciente.vue';
@@ -8,6 +10,7 @@
   const pacientes = ref([]);
 
   const paciente = reactive({
+        id: null,
         nombre: '',
         propietario: '',
         email: '',
@@ -17,7 +20,8 @@
 
     const guardarPaciente = () => {
       pacientes.value.push({
-        ...paciente
+        ...paciente,
+        id: uid()
       })
 
       // paciente.nombre = ''
